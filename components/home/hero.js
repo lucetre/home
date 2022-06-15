ea47fd29d89b065f2009ba9d6e7333c3847fb70f
lucetre <lucetre@naver.com>
2022-06-15 19:13:59 +0900

    Erase asp

--- components/home/hero.js
+++ components/home/hero.js
@@ -10,7 +10,7 @@ export default function Hero() {
         </h1>
         <p className="mb-8 leading-relaxed">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
         </p>
