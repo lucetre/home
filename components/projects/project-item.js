0b6feb7f01918c4247a6bd5447746b72227c9af5
lucetre <lucetre.naver.com>
2022-06-15 16:45:55 +0900

    Revise boilerplate code

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -2,31 +2,12 @@ import Image from "next/image";
 
 export default function ProjectItem({data}){
 
-    const title = data.properties.Name.title[0].plain_text
-    const github = data.properties.Github.url
-    const youtube = data.properties.Youtube.url
-    const description = data.properties.Description.rich_text[0].plain_text
+    const title = data.properties.Name?.title[0].plain_text
+    const github = data.properties.Github?.url
+    const youtube = data.properties.Youtube?.url
+    const description = data.properties.Description?.rich_text[0].plain_text
     const imgSrc = data.cover.file?.url || data.cover.external.url
-    const tags = data.properties.Tags.multi_select
-    const start = data.properties.WorkPeriod.date.start
-    const end = data.properties.WorkPeriod.date.end
-
-    const calculatedPeriod = (start, end) => {
-        const startDateStringArray = start.split('-');
-        const endDateStringArray = end.split('-');
-
-        var startDate = new Date(startDateStringArray[0], startDateStringArray[1], startDateStringArray[2]);
-        var endDate = new Date(endDateStringArray[0], endDateStringArray[1], endDateStringArray[2]);
-
-        console.log(`startDate: ${startDate}`)
-        console.log(`endDate: ${endDate}`)
-
-        const diffInMs = Math.abs(endDate - startDate);
-        const result = diffInMs / (1000 * 60 * 60 * 24);
-
-        console.log(`기간 : ${result}`)
-        return result;
-    };
+    const tags = data.properties.Tags?.multi_select
 
     return (
         <div className="project-card">
@@ -46,11 +27,8 @@ export default function ProjectItem({data}){
                 <h3 className="mt-4 text-xl">{description}</h3>
                 <a href={github}>깃허브 바로가기</a>
                 <a href={youtube}>유튜브 시연영상 보러가기</a>
-                <p className="my-1 ">
-                    작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
-                </p>
                 <div className="flex items-start mt-2">
-                    {tags.map((aTag) => (
+                    {tags?.map((aTag) => (
                         <h1 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30" key={aTag.id}>{aTag.name}</h1>
                     ))}
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Layout from '../components/layout';
 import Hero from '../components/home/hero';
-import Animation from '../components/home/animation';
 
 export default function Home() {
   return (
     <Layout>
         <Head>
-            <title>빡코딩 포트폴리오</title>
-            <meta name="description" content="오늘도 빡코딩!" />
+            <title>Portfolio</title>
+            <meta name="description" content="lucetre portfolio" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
         <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
