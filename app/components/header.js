c2ea860ff27d9034f329013da210df3b875c9841
Sangjun Son <lucetre@naver.com>
2022-06-18 19:48:44 +0900

    Update header.js

--- app/components/header.js
+++ app/components/header.js
@@ -9,10 +9,9 @@ export default function Header() {
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link href="/">
             <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-              <Image
+              <img
                 src="favicon.png"
                 style={{ width: 35, height: 35 }}
-                alt="image"
               />
               <span className="ml-3 text-xl">My Portfolio</span>
             </a>
